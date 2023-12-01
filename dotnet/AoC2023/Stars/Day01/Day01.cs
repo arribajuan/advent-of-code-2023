695014fe349d2cd0b3207026d8c6dc891231adc4
Juan Martinez <jmartinez@flinks.com>
2023-12-01 08:55:18 -0500

    Day 1 - star 1 implementation + unit tests - calibration total

--- dotnet/AoC2023/Stars/Day01/Day01.cs
+++ dotnet/AoC2023/Stars/Day01/Day01.cs
@@ -28,4 +28,20 @@ public class Day01
         var result = Convert.ToInt32(firstNumber + lastNumber);
         return result;
     }
+
+    public int SumCalibrationValuesFromFile()
+    {
+        var totalCalibrationValue = 0;
+        
+        var ddataFilePath = AppDomain.CurrentDomain.BaseDirectory + "assets/data/day01-test.txt";
+        var fio = new FileIO();
+        var textLines = fio.LoadTextLinesFromFile(ddataFilePath);
+
+        foreach (var textLine in textLines)
+        {
+            totalCalibrationValue += FindCalibrationValue(textLine);
+        }
+
+        return totalCalibrationValue;
+    }
 }
--- dotnet/AoC2023/Stars/Day01/Day01Test.cs
+++ dotnet/AoC2023/Stars/Day01/Day01Test.cs
@@ -7,9 +7,18 @@ public class Day01Test
     [InlineData("pqr3stu8vwx", 38)]
     [InlineData("a1b2c3d4e5f", 15)]
     [InlineData("treb7uchet", 77)]
-    public void Test_Day01_CalculateFinalFloor_SampleDirections(string text, int calibrationValue)
+    public void Test_Day01_FindCalibrationValues(string text, int calibrationValue)
     {
         var d01 = new Day01();
         Assert.Equal(calibrationValue , d01.FindCalibrationValue(text));
+    }
+    
+    [Fact]
+    public void Test_Day01_FindCalibrationValues_FromFile()
+    {
+        var d01 = new Day01();
+        var totalCalibrationValue = d01.SumCalibrationValuesFromFile();
+        
+        Assert.Equal(142, totalCalibrationValue);
     }
 }
