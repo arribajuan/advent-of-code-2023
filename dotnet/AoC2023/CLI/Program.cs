0759181b851d95683eaf9bea2f97ef393df1c62b
Juan Martinez <jmartinez@flinks.com>
2023-12-01 09:29:27 -0500

    Day 1 - star 1 solution

--- dotnet/AoC2023/CLI/Program.cs
+++ dotnet/AoC2023/CLI/Program.cs
@@ -1,3 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, AoC 2023!");
+Console.WriteLine("Hello there!");
+Console.WriteLine("-----------------------------");
+
+#region Day 1
+
+var d01 = new Stars.Day01.Day01();
+var d01SumCalibrationValues = d01.SumCalibrationValuesFromFile();
+
+Console.WriteLine("Day 1");
+Console.WriteLine($" Star 1 - CalibrationValues = {d01SumCalibrationValues}");
+Console.WriteLine("-----------------------------");
+
+#endregion
