8241ed448b13e7b30509fa28652b4166fb2dbbff
Juan Martinez <jmartinez@flinks.com>
2023-12-01 08:49:56 -0500

    Day 1 - star 1 unit tests - calibration value

--- dotnet/AoC2023/Stars/Day01/Day01Test.cs
+++ dotnet/AoC2023/Stars/Day01/Day01Test.cs
@@ -2,5 +2,14 @@ namespace Stars.Day01;
 
 public class Day01Test
 {
-    
+    [Theory]
+    [InlineData("1abc2", 12)]
+    [InlineData("pqr3stu8vwx", 38)]
+    [InlineData("a1b2c3d4e5f", 15)]
+    [InlineData("treb7uchet", 77)]
+    public void Test_Day01_CalculateFinalFloor_SampleDirections(string text, int calibrationValue)
+    {
+        var d01 = new Day01();
+        Assert.Equal(calibrationValue , d01.FindCalibrationValue(text));
+    }
 }
